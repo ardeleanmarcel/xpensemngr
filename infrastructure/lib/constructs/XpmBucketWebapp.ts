d1bf22fc1d3e2d7590b4a16be86caa8e76502e2b
Valentin Harapu <valentin.harapu@xpensemngr.com>
2024-10-05 18:13:07 +0300

    Update webapp s3 bucket config

--- infrastructure/lib/constructs/XpmBucketWebapp.ts
+++ infrastructure/lib/constructs/XpmBucketWebapp.ts
@@ -1,39 +1,41 @@
 import * as cdk from "aws-cdk-lib";
 import * as s3 from "aws-cdk-lib/aws-s3";
+import * as iam from "aws-cdk-lib/aws-iam";
 import { Construct } from "constructs";
 
-export class XpmBucketWebapp extends s3.Bucket {
+export class XpmBucketWebapp extends Construct {
+  public readonly bucket: s3.IBucket;
+  public readonly policy: iam.PolicyStatement;
+
   constructor(scope: Construct, id: string) {
     const bucketId = `xpm-bucket-${id}`;
 
-    super(scope, bucketId, {
+    super(scope, `${bucketId}-with-policy`);
+
+    this.bucket = new s3.Bucket(scope, bucketId, {
       bucketName: bucketId,
       versioned: false,
-      blockPublicAccess: new s3.BlockPublicAccess({
-        blockPublicAcls: false,
-        restrictPublicBuckets: false,
-        blockPublicPolicy: false,
-        ignorePublicAcls: false,
-      }),
       publicReadAccess: true,
+      blockPublicAccess: {
+        blockPublicPolicy: false,
+        blockPublicAcls: false,
+        ignorePublicAcls: false,
+        restrictPublicBuckets: false,
+      },
       websiteIndexDocument: "index.html",
       websiteErrorDocument: "index.html",
-      websiteRoutingRules: [
-        {
-          condition: {
-            httpErrorCodeReturnedEquals: "404",
-          },
-          replaceKey: s3.ReplaceKey.with("index.html"),
-        },
-        {
-          condition: {
-            httpErrorCodeReturnedEquals: "403",
-          },
-          replaceKey: s3.ReplaceKey.with("index.html"),
-        },
-      ],
       removalPolicy: cdk.RemovalPolicy.DESTROY,
       autoDeleteObjects: true,
     });
+
+    // TODO (Valle) -> check if this policy is really needed
+    this.policy = new iam.PolicyStatement({
+      effect: iam.Effect.ALLOW,
+      actions: ["s3:GetObject"],
+      principals: [new iam.AnyPrincipal()],
+      resources: [`arn:aws:s3:::${bucketId}/*`],
+    });
+
+    this.bucket.addToResourcePolicy(this.policy);
   }
 }
