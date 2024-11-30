3a6a413b8dfb58f031404fad9a5dedc346b6ab7c
Valentin Harapu <valentin.harapu@xpensemngr.com>
2024-11-30 14:13:40 +0200

    add mixin

--- webapp/src/components/info/XpmLoadingSpinner/XpmLoadingSpinner.tsx
+++ webapp/src/components/info/XpmLoadingSpinner/XpmLoadingSpinner.tsx
@@ -20,7 +20,7 @@ export function XpmLoadingSpinner() {
         <span>€</span>
         <span>£</span>
         <span>¥</span>
-        <span>₩</span>
+        <span>₱</span>
       </div>
     </div>
   );
