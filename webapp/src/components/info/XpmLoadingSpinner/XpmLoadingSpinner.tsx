e9d02c1dc2810dc9216ec1ec05ac4c993788961f
Valentin Harapu <valentin.harapu@xpensemngr.com>
2024-11-30 13:53:11 +0200

    Upate to 3 currencies

--- webapp/src/components/info/XpmLoadingSpinner/XpmLoadingSpinner.tsx
+++ webapp/src/components/info/XpmLoadingSpinner/XpmLoadingSpinner.tsx
@@ -19,6 +19,8 @@ export function XpmLoadingSpinner() {
         <span>$</span>
         <span>€</span>
         <span>£</span>
+        <span>¥</span>
+        <span>₩</span>
       </div>
     </div>
   );
