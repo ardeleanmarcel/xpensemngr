f6bb9537b011fffaa4581d8a7ae282d5c4c13e28
Valentin Harapu <valentin.harapu@xpensemngr.com>
2024-12-25 22:12:19 +0200

    Enable layout change on window resize

--- webapp/src/components/layout/AppLayout/AppLayout.tsx
+++ webapp/src/components/layout/AppLayout/AppLayout.tsx
@@ -1,9 +1,29 @@
-import React from 'react';
+import React, { useCallback, useEffect, useState } from 'react';
 
+import { useDebounced } from '../../../hooks/useDebounced';
 import { DesktopLayout } from './components/DesktopLayout/DesktopLayout';
 
+const MAX_MOBILE_WIDTH = 800;
+
 export const AppLayout: React.FunctionComponent<React.PropsWithChildren> = ({ children }) => {
-  if (window.innerWidth < 800) return <>{children}</>;
+  const [isMobile, setIsMobile] = useState(window.innerWidth < MAX_MOBILE_WIDTH);
+  const debounced = useDebounced(500);
+
+  const handleResize = useCallback(() => {
+    debounced.run(() => {
+      const isMob = window.innerWidth < MAX_MOBILE_WIDTH;
+
+      setIsMobile(isMob);
+    });
+  }, [debounced]);
+
+  useEffect(() => {
+    window.addEventListener('resize', handleResize);
+
+    return () => window.removeEventListener('resize', handleResize);
+  });
+
+  if (isMobile) return <>{children}</>;
 
   return <DesktopLayout>{children}</DesktopLayout>;
 };
