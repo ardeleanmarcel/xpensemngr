3d8ae57ed1c8855befd969d0ef503c6a956e98a0
Valentin Harapu <valentin.harapu@xpensemngr.com>
2024-12-25 22:39:15 +0200

    Add account settings button to top bar

--- webapp/src/components/layout/AppLayout/components/LayoutMobile/LayoutMobile.tsx
+++ webapp/src/components/layout/AppLayout/components/LayoutMobile/LayoutMobile.tsx
@@ -2,13 +2,15 @@ import './LayoutMobile.scss';
 
 import React from 'react';
 
+import { AccountSettings } from '../../../../../Pages/Expenses/AddExpenses/AccountSettings';
 import { ThreeLines } from '../../../../icons/ThreeLines/ThreeLines';
+import { ButtonPill } from '../../../../input/ButtonPill/ButtonPill';
 
 // TODO (Valle) -> create an ButtonIcon component that takes an icon as a prop
 export const LayoutMobile: React.FunctionComponent<React.PropsWithChildren> = ({ children }) => {
   return (
     <div className="LayoutMobile">
-      <div className="LayoutMobile--menu">
+      <div className="LayoutMobile--top-bar">
         <button
           onClick={() => {
             console.log('menu');
@@ -16,6 +18,13 @@ export const LayoutMobile: React.FunctionComponent<React.PropsWithChildren> = ({
         >
           <ThreeLines />
         </button>
+        <ButtonPill
+          text="Add Expense" // TODO -> change name and functionality based on current page
+          onClick={() => {
+            console.log('add expense');
+          }}
+        />
+        <AccountSettings />
       </div>
       <div className="LayoutMobile--content">{children}</div>
     </div>
