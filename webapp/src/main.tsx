3098c91e4b7ebf8eb1cbcfd4c765ca4adb00dd75
Valentin Harapu <valentin.harapu@xpensemngr.com>
2025-02-13 08:48:54 +0200

    Test deployment

--- webapp/src/main.tsx
+++ webapp/src/main.tsx
@@ -4,3 +4,5 @@ import App from './App.tsx';
 
 // Warn! Adding <React.StrictMode> here will break the internal events system.
 ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
+
+// test-change
