4408873a7773d0085707a91e67acf71e379183b7
Valentin Harapu <valentin.harapu@xpensemngr.com>
2025-03-01 18:23:56 +0200

    Wrap db query in try/catch

--- api/src/adapters/sqlClient.ts
+++ api/src/adapters/sqlClient.ts
@@ -1,5 +1,8 @@
 import { Knex } from 'knex';
 import { knexClient } from '@src/db/client.ts';
+import { log } from '@xpm/logging';
+import { HTTP_ERR } from '@src/errors/http.errors.ts';
+import { throwHttpError } from '@src/errors/error.utils.ts';
 
 type QueryParams = Array<string | number | null | Array<string | number>>;
 
@@ -9,12 +12,16 @@ class SqlClient {
     this.client = knexClient;
   }
 
-  // TODO (Valle) -> client queries should be wrapped in try/catch and return an appropriate HttpError
-
   public async query<T>(statement: string, params: QueryParams = []) {
-    const res = await this.client.raw(statement, params);
+    try {
+      const res = await this.client.raw(statement, params);
 
-    return res.rows as T[];
+      return res.rows as T[];
+    } catch (error) {
+      log.error(`Could not execute query:\n${statement}`);
+      if (error instanceof Error) log.error(`Reason: ${error.message || 'unknown'}`);
+      throwHttpError(HTTP_ERR.e500.Unavailable);
+    }
   }
 }
 
