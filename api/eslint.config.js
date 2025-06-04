d417d9861b9ba77dd6b19c8a31c54c073cf68672
Valentin Harapu <valentin.harapu@xpensemngr.com>
2025-06-04 19:19:19 +0300

    Add some eslint ignores

--- api/eslint.config.js
+++ api/eslint.config.js
@@ -4,6 +4,9 @@ import tseslint from 'typescript-eslint';
 
 const config = [
   {
+    ignores: ['**/node_modules/**', '**/dist/**', '**/build/**', '.dependency-cruiser.cjs', 'eslint.config.js'],
+  },
+  {
     rules: {
       'no-undef': 'error',
       '@typescript-eslint/no-unnecessary-condition': 'error',
@@ -13,7 +16,6 @@ const config = [
   {
     languageOptions: {
       globals: globals.browser,
-
       parserOptions: {
         project: './tsconfig.json',
       },
