454ccec5d6057fb1853d2b843c6ecd3d99f2999a
Valentin Harapu <valentin.harapu@xpensemngr.com>
2025-06-13 18:54:40 +0300

    chore(cleanup): make use of `useScreenSize` hook in App layout

--- webapp/src/components/layout/AppLayout/AppLayout.tsx
+++ webapp/src/components/layout/AppLayout/AppLayout.tsx
@@ -1,30 +1,14 @@
-import React, { useCallback, useEffect, useState } from 'react';
+import React from 'react';
 
-import { useDebounced } from '../../../hooks/useDebounced';
+import { SCREEN_SIZE } from '../../../constants/screenSize';
+import { useScreenSize } from '../../../hooks/useScreenSize';
 import { DesktopLayout } from './components/DesktopLayout/DesktopLayout';
 import { LayoutMobile } from './components/LayoutMobile/LayoutMobile';
 
-const MAX_MOBILE_WIDTH = 800;
-
 export const AppLayout: React.FunctionComponent<React.PropsWithChildren> = ({ children }) => {
-  const [isMobile, setIsMobile] = useState(window.innerWidth < MAX_MOBILE_WIDTH);
-  const debounced = useDebounced(500);
+  const { screenSize } = useScreenSize();
 
-  const handleResize = useCallback(() => {
-    debounced.run(() => {
-      const isMob = window.innerWidth < MAX_MOBILE_WIDTH;
-
-      setIsMobile(isMob);
-    });
-  }, [debounced]);
-
-  useEffect(() => {
-    window.addEventListener('resize', handleResize);
-
-    return () => window.removeEventListener('resize', handleResize);
-  });
-
-  if (isMobile) return <LayoutMobile>{children}</LayoutMobile>;
+  if (screenSize === SCREEN_SIZE.Phone) return <LayoutMobile>{children}</LayoutMobile>;
 
   return <DesktopLayout>{children}</DesktopLayout>;
 };
