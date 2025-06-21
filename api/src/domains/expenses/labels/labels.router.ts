8cf3459349245b03b6697bb2ed6941d4c2087264
Valentin Harapu <valentin.harapu@xpensemngr.com>
2025-06-21 11:13:42 +0300

    feat(labels): add endpoint for batch update

--- api/src/domains/expenses/labels/labels.router.ts
+++ api/src/domains/expenses/labels/labels.router.ts
@@ -1,13 +1,40 @@
 import { protectedProcedure, t } from '../../../trpc.ts';
-import { labelCreateSchema } from './labels.models.ts';
-import { AllowedLabelsFilters, createLabels, selectLabels } from './labels.sql.ts';
+import { labelCreateSchema, labelUpdateSchema } from './labels.models.ts';
+import {
+  AllowedLabelsFilters,
+  checkLabelsBelongToUser,
+  createLabels,
+  selectLabels,
+  updateLabels,
+} from './labels.sql.ts';
 import { Filter, FILTER_COMPARATOR } from '../../../services/database/database.utils.ts';
+import { throwHttpError } from '../../../services/error/error.utils.ts';
+import { HTTP_ERR } from '../../../services/error/http.errors.ts';
 
 export const labelsRouter = t.router({
   create: protectedProcedure.input(labelCreateSchema).mutation(async (opts) => {
     const { user } = opts.ctx;
 
     const result = createLabels(opts.input, user.user_id);
+
+    return result;
+  }),
+
+  // TODO (Valle) -> allow for partial updates
+  update: protectedProcedure.input(labelUpdateSchema).mutation(async (opts) => {
+    const { user } = opts.ctx;
+    const { input: labels } = opts;
+
+    const allLablesBelongToUser = await checkLabelsBelongToUser(
+      labels.map((label) => label.label_id),
+      user.user_id
+    );
+
+    if (!allLablesBelongToUser) {
+      throwHttpError(HTTP_ERR.e400.BadRequest(`User does not have access to requested label(s).`));
+    }
+
+    const result = await updateLabels(labels);
 
     return result;
   }),
